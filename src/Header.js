9790f0d2ed35285eb06ebcbb505e70c1363a6e8e
unknown <573315545lena@gmail.com>
2023-04-29 08:08:32 +0200

    Added new files Footer.js, Header.js, Main.js, Nav.js, index.html, add imsge logo.png, some changes in App.js, added a links in Nav.js and Header.js

---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Nav from './Nav';
+import logo from './logo.png';
+
+function Header() {
+    return (
+    <header>
+      <h1>My Website</h1>
+      <img src={logo} alt="Logo"/>
+      <Nav />
+    </header>
+   );
+}
+export default Header
+
+
+
+
